9a01ffb5a9d469de038717459c1ffd678d44aa68
luvanidixit101 <luvanidixit101@gmail.com>
2026-09-18 22:51:59 +0530

    chore: configure environment-based django settings

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -11,21 +11,38 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
+env = environ.Env(
+    DJANGO_DEBUG=(bool, False),
+)
 
+environ.Env.read_env(BASE_DIR.parent / ".env")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sr7vcj6l^cg%nbpbg_bq1ggz+91ezjt%$ag7u#z6(jfwx-%fvr'
+# # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = 'django-insecure-sr7vcj6l^cg%nbpbg_bq1ggz+91ezjt%$ag7u#z6(jfwx-%fvr'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+
+
+SECRET_KEY = env("DJANGO_SECRET_KEY")
+
+DEBUG = env.bool("DJANGO_DEBUG")
+
+ALLOWED_HOSTS = env.list(
+    "DJANGO_ALLOWED_HOSTS",
+    default=["localhost", "127.0.0.1"],
+)
+
+
 
 
 # Application definition
